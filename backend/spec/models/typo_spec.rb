45e0528b41af460b3d6e554e698e725bc5480d6b
Kuroneko <ken.aster8@gmail.com>
2025-11-24 16:52:02 +0900

    各削除メソッドのテストを追加

--- backend/spec/models/typo_spec.rb
+++ backend/spec/models/typo_spec.rb
@@ -38,4 +38,39 @@ RSpec.describe Typo, type: :model do
       end
     end
   end
+
+  describe '.delete_by_repository' do
+    let(:user) { create(:user) }
+    let!(:first_repository) { create(:repository, user:) }
+    let!(:second_repository) { create(:repository, user:) }
+
+    before do
+      create(:file_item, :with_typing_progress_and_typos, repository: first_repository)
+      create(:file_item, :with_typing_progress_and_typos, repository: second_repository)
+    end
+
+    context 'when single repository_id is given' do
+      it 'deletes all typos for the repository' do
+        expect do
+          described_class.delete_by_repository(first_repository.id)
+        end.to change(described_class, :count).by(-2)
+      end
+    end
+
+    context 'when array of repository_ids is given' do
+      it 'deletes all typos for the repositories' do
+        expect do
+          described_class.delete_by_repository([first_repository.id, second_repository.id])
+        end.to change(described_class, :count).by(-4)
+      end
+    end
+
+    context 'when empty array is given' do
+      it 'does not delete any typos' do
+        expect do
+          described_class.delete_by_repository([])
+        end.not_to change(described_class, :count)
+      end
+    end
+  end
 end
