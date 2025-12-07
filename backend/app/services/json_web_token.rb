7e71696ce97f0fbabc87c1c78f6a65d6d8722d8a
Kuroneko <ken.aster8@gmail.com>
2025-12-07 18:00:49 +0900

    定数を削除と、rescueの意図をコメント

--- backend/app/services/json_web_token.rb
+++ backend/app/services/json_web_token.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.secret_key_base
-
   def self.encode(user_id, expires_at)
     payload = { user_id:, exp: expires_at.to_i }
-    JWT.encode(payload, SECRET_KEY) # デフォルトではHS256アルゴリズムが使用される
+    JWT.encode(payload, Rails.application.credentials.secret_key_base) # デフォルトではHS256アルゴリズムが使用される
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+    decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
     ActiveSupport::HashWithIndifferentAccess.new(decoded)
   rescue JWT::DecodeError => e
+    # トークンの有効切れの際はnilを返す
     LogUtils.log_error(e, 'JsonWebToken.decode')
     nil
   end
