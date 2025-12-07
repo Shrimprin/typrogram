40abacb7de430fc62c6b1f68c5b3b8a26adc4037
Kuroneko <ken.aster8@gmail.com>
2025-12-07 15:52:05 +0900

    サインインをresouceのcreateアクションでで呼べるように修正した

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  post '/api/auth/callback/github', to: 'api/auth#login'
+  # post '/api/auth/callback/github', to: 'api/auth#login'
 
   namespace :api do
+    resource :login, only: [:create], controller: 'login'
     resources :users, only: [:destroy]
     get '/repositories/preview', to: 'repositories/previews#show'
     resources :repositories, only: [:index, :show, :create, :destroy] do
