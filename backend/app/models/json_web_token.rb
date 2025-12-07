923cb45f48459ec375319ebcde2f0e58c03794f4
Kuroneko <ken.aster8@gmail.com>
2025-12-07 18:59:31 +0900

    クラスメソッドをclsss << selfで囲って分かりやすくした

--- backend/app/models/json_web_token.rb
+++ backend/app/models/json_web_token.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class JsonWebToken
-  def self.encode(user_id, expires_at)
-    payload = { user_id:, exp: expires_at.to_i }
-    JWT.encode(payload, Rails.application.credentials.secret_key_base) # デフォルトではHS256アルゴリズムが使用される
-  end
+  class << self
+    def encode(user_id, expires_at)
+      payload = { user_id:, exp: expires_at.to_i }
+      JWT.encode(payload, Rails.application.credentials.secret_key_base) # デフォルトではHS256アルゴリズムが使用される
+    end
 
-  def self.decode(token)
-    decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
-    ActiveSupport::HashWithIndifferentAccess.new(decoded)
-  rescue JWT::DecodeError => e
-    # トークンの有効切れの際はnilを返す
-    LogUtils.log_error(e, 'JsonWebToken.decode')
-    nil
+    def decode(token)
+      decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      ActiveSupport::HashWithIndifferentAccess.new(decoded)
+    rescue JWT::DecodeError => e
+      # トークンの有効切れの際はnilを返す
+      LogUtils.log_error(e, 'JsonWebToken.decode')
+      nil
+    end
   end
 end
